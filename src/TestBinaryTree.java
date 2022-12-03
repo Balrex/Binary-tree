4dee010c5c087f228a28db6bfee4c9b3955d2a04
Борис <118976669+Balrex@users.noreply.github.com>
2022-12-03 22:27:58 +0300

    Create TestBinaryTree.java

---
+++ src/TestBinaryTree.java
@@ -0,0 +1,18 @@
+public class TestBinaryTree {
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.AddEl(10);
+        bt.AddEl(7);
+        bt.AddEl(15);
+        bt.AddEl(13);
+        bt.AddEl(20);
+        bt.AddEl(9);
+        bt.AddEl(17);
+        bt.AddEl(4);
+        bt.AddEl(8);
+        bt.AddEl(1);
+        bt.AddEl(6);
+        bt.AddEl(5);
+        bt.MinMax();
+    }
+}
